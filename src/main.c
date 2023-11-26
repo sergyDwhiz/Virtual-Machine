125818108004b9c65f26ad5d46064185e3ff7d08
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-11-26 02:38:11 +0100

    Howto file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 // Examines output from header & virtual files.
 #include <stdio.h>
-#include "src/vm.h"
+#include "../src/vm.h" 
 
 int main() {
     // Initialize the virtual machine
-    VM vm;
+    VM vm; // Declare an instance of the VM type
     vm_init(&vm);
 
     // Load and execute the program
@@ -16,3 +16,16 @@ int main() {
 
     return 0;
 }
+    VM vm; // Declare an instance of the VM type
+
+    // Load and execute the program
+    vm_init(&vm);
+    vm_load_program(&vm, "program.bin");
+    vm_execute(&vm);
+
+    // Cleanup
+    vm_cleanup(&vm);
+
+    return 0;
+}
+
