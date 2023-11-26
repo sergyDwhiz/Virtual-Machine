6bf23679e68117c0dc9df1d87fc148669741f2c7
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-11-26 02:49:05 +0100

    File for JIT compiler

---
+++ src/JIT_Compiler.c
@@ -0,0 +1,7 @@
+// Just In Time Compiler. 
+//
+// Contains the implementation of the JIT (Just-In-Time) Compiler module.
+// responsible for dynamically compiling and optimizing code at runtime,
+// allowing for faster execution of the virtual machine.
+//
+// 
