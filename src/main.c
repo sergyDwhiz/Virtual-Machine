b4dbaf0a65fa13cf0596033dc0ee3e4a2488090c
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-11-26 02:55:42 +0100

    Load and execute files

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
-// Examines output from header & virtual files.
 #include <stdio.h>
 #include "../src/vm.h" 
 
 int main() {
     // Initialize the virtual machine
-    VM vm; // Declare an instance of the VM type
+    VM vm;
     vm_init(&vm);
 
     // Load and execute the program
@@ -16,16 +15,3 @@ int main() {
 
     return 0;
 }
-    VM vm; // Declare an instance of the VM type
-
-    // Load and execute the program
-    vm_init(&vm);
-    vm_load_program(&vm, "program.bin");
-    vm_execute(&vm);
-
-    // Cleanup
-    vm_cleanup(&vm);
-
-    return 0;
-}
-
